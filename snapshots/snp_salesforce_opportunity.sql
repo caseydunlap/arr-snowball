c9ffdf4c5cd84e3fb61a584c6b5d3724e278a471
mdunlap@hhaexchange.com <mdunlap@hhaexchange.com>
2023-04-03 20:38:37 +0000

    init

---
+++ snapshots/snp_salesforce_opportunity.sql
@@ -0,0 +1,17 @@
+{% snapshot snp_salesforce_opportunity %}
+
+{{
+    config(
+      target_database='pc_fivetran_db',
+      target_schema='dbt_caseydunlap',
+      unique_key='id',
+      strategy='timestamp',
+      updated_at='last_modified_date',
+    )
+}}
+
+select * 
+, current_timestamp() as dbt_snapshot_at
+from {{ source('salesforce', 'opportunity') }}
+
+{% endsnapshot %}
